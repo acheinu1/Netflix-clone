b45146724e80de005eb1825d3569842e6c92ec4f
acheinu1 <acheinue@gmail.com>
2020-10-28 18:48:24 +0100

    added the footer component

--- my-app/src/parent.jsx
+++ my-app/src/parent.jsx
@@ -1,24 +1,12 @@
-import React from 'react'
-import Jumbotron from "./components/jumbotron"
-import Jumbodata from "./fixtures/jumbo"
+import React from 'react';
+import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from "./containers/footer"
+
 export default function Parent() {
     return (
         <>
-            <Jumbotron.Container>
-                {Jumbodata.map((item) =>(
-                <Jumbotron key={item.id} direction ={item.direction}> 
-                  {/* left-----------------right */}
-                    <Jumbotron.Pane>
-                        <Jumbotron.Title>{item.title}</Jumbotron.Title>
-                        <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
-                    </Jumbotron.Pane>
-                    {/* right---------------left */}
-                    <Jumbotron.Pane>
-                        <Jumbotron.Image  src = {item.image} alt = {item.alt} />
-                    </Jumbotron.Pane>
-                </Jumbotron>
-                ))}
-            </Jumbotron.Container>
+           <JumbotronContainer/>
+           <FooterContainer />
         </>
     );
 }
