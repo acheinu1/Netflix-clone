da3812b6a739598f4998e3d9b6f8513570890bf5
acheinu1 <acheinue@gmail.com>
2020-10-29 18:03:31 +0100

    finished the Accordion

--- my-app/src/parent.jsx
+++ my-app/src/parent.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from "./containers/footer"
+import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs"
 
 export default function Parent() {
     return (
         <>
            <JumbotronContainer/>
+           <FaqsContainer />
            <FooterContainer />
         </>
     );
